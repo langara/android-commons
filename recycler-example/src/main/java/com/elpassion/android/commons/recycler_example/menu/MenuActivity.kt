3d3e9d89ff3917f60f2d5909c4299558c6cf138f
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-02 15:03:55 +0100

    Use BasicAdapter in MenuActivity

--- recycler-example/src/main/java/com/elpassion/android/commons/recycler_example/menu/MenuActivity.kt
+++ recycler-example/src/main/java/com/elpassion/android/commons/recycler_example/menu/MenuActivity.kt
@@ -3,13 +3,15 @@ package com.elpassion.android.commons.recycler_example.menu
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import com.elpassion.android.commons.recycler.adapters.recyclerViewAdapter
+import com.elpassion.android.commons.recycler.adapters.basicAdapterWithLayoutAndBinder
+import com.elpassion.android.commons.recycler.basic.asBasicList
 import com.elpassion.android.commons.recycler_example.R
 import com.elpassion.android.commons.recycler_example.group.MutableRecyclerWithSectionsActivity
 import com.elpassion.android.commons.recycler_example.group.RecyclerWithSectionActivity
 import com.elpassion.android.commons.recycler_example.group.StableMutableRecyclerWithSectionsActivity
 import com.elpassion.android.commons.recycler_example.list.BasicListActivity
 import com.elpassion.android.commons.recycler_example.list.SimpleListActivity
+import kotlinx.android.synthetic.main.example_item.view.*
 import kotlinx.android.synthetic.main.recycler_view.*
 
 class MenuActivity : AppCompatActivity() {
@@ -25,7 +27,11 @@ class MenuActivity : AppCompatActivity() {
                 ExampleItem(name = RecyclerWithSectionActivity.DESCRIPTION, onClick = { RecyclerWithSectionActivity.start(this) }),
                 ExampleItem(name = MutableRecyclerWithSectionsActivity.DESCRIPTION, onClick = { MutableRecyclerWithSectionsActivity.start(this) }),
                 ExampleItem(name = StableMutableRecyclerWithSectionsActivity.DESCRIPTION, onClick = { StableMutableRecyclerWithSectionsActivity.start(this) })
-        )
-        recyclerView.adapter = recyclerViewAdapter(examples.map(::ExampleItemAdapter))
+        ).asBasicList()
+
+        recyclerView.adapter = basicAdapterWithLayoutAndBinder(examples, R.layout.example_item) { holder, item ->
+            holder.itemView.example_name.text = item.name
+            holder.itemView.setOnClickListener { item.onClick() }
+        }
     }
 }
