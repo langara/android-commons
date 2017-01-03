83a93801b9c65fcb43305fb33075a8305acf0e48
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-03 16:53:10 +0100

    Add clear method to BasicMutableMap

--- recycler/src/main/java/com/elpassion/android/commons/recycler/basic/BasicMutableMap.kt
+++ recycler/src/main/java/com/elpassion/android/commons/recycler/basic/BasicMutableMap.kt
@@ -3,4 +3,5 @@ package com.elpassion.android.commons.recycler.basic
 
 interface BasicMutableMap<Key, Value> : BasicMap<Key, Value> {
     operator fun set(key: Key, value: Value)
+    fun clear()
 }
