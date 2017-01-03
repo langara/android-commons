84cc030afc958ccd987d1e40d288f546c5b50f58
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-03 09:52:50 +0100

    BasicAdapter items: val -> var

--- recycler/src/main/java/com/elpassion/android/commons/recycler/basic/BasicAdapter.kt
+++ recycler/src/main/java/com/elpassion/android/commons/recycler/basic/BasicAdapter.kt
@@ -4,7 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 
 
-abstract class BasicAdapter<V : View, I>(val items: BasicList<I>) : RecyclerView.Adapter<BasicViewHolder<V, I>>() {
+abstract class BasicAdapter<V : View, I>(var items: BasicList<I>) : RecyclerView.Adapter<BasicViewHolder<V, I>>() {
 
     override fun onBindViewHolder(holder: BasicViewHolder<V, I>, position: Int) = holder.bind(items[position])
 
