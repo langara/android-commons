8e3a1ca34ad5a52c26fbac86fd3c71740bd2eed7
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-03 14:22:22 +0100

    Add stable id support to BasicAdapter

--- recycler/src/main/java/com/elpassion/android/commons/recycler/basic/BasicAdapter.kt
+++ recycler/src/main/java/com/elpassion/android/commons/recycler/basic/BasicAdapter.kt
@@ -1,6 +1,7 @@
 package com.elpassion.android.commons.recycler.basic
 
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.NO_ID
 import android.view.View
 
 
@@ -9,4 +10,6 @@ abstract class BasicAdapter<V : View, I>(var items: BasicList<I>) : RecyclerView
     override fun onBindViewHolder(holder: BasicViewHolder<V, I>, position: Int) = holder.bind(items[position])
 
     override fun getItemCount() = items.size
+
+    override fun getItemId(position: Int) = (items[position] as? WithStableId)?.id ?: NO_ID
 }
