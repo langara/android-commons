1ae19a309d065faadbd28262e42c48df29fc3413
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-03 18:39:34 +0100

    Add insert and remove methods to BasicMutableList

--- recycler/src/main/java/com/elpassion/android/commons/recycler/basic/BasicMutableList.kt
+++ recycler/src/main/java/com/elpassion/android/commons/recycler/basic/BasicMutableList.kt
@@ -1,5 +1,8 @@
 package com.elpassion.android.commons.recycler.basic
 
 
-interface BasicMutableList<Item> : BasicList<Item>, BasicMutableMap<Int, Item>
+interface BasicMutableList<Item> : BasicList<Item>, BasicMutableMap<Int, Item> {
+    fun insert(index: Int, item: Item)
+    fun remove(index: Int)
+}
 
