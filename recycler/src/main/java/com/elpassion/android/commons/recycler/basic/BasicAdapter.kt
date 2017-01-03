0feaa0693cfa028ad6c62af72feb2308093ee1b9
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-03 09:51:14 +0100

    [Refactoring] Remove some open functions definitions from BasicAdapter

--- recycler/src/main/java/com/elpassion/android/commons/recycler/basic/BasicAdapter.kt
+++ recycler/src/main/java/com/elpassion/android/commons/recycler/basic/BasicAdapter.kt
@@ -2,14 +2,9 @@ package com.elpassion.android.commons.recycler.basic
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.view.ViewGroup
 
 
 abstract class BasicAdapter<V : View, I>(val items: BasicList<I>) : RecyclerView.Adapter<BasicViewHolder<V, I>>() {
-
-    override fun getItemViewType(position: Int) = 0
-
-    override abstract fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BasicViewHolder<V, I>
 
     override fun onBindViewHolder(holder: BasicViewHolder<V, I>, position: Int) = holder.bind(items[position])
 
