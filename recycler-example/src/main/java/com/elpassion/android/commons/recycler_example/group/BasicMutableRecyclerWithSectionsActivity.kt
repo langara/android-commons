cb15906dfde7135256fa17fe901aa2e6b9c5406e
Marcin Suska <marcin.suska@elpassion.pl>
2017-01-05 12:50:52 +0100

    [Refactoring] Replace if by when with key to highlight create view holder action

--- recycler-example/src/main/java/com/elpassion/android/commons/recycler_example/group/BasicMutableRecyclerWithSectionsActivity.kt
+++ recycler-example/src/main/java/com/elpassion/android/commons/recycler_example/group/BasicMutableRecyclerWithSectionsActivity.kt
@@ -3,6 +3,7 @@ package com.elpassion.android.commons.recycler_example.group
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import com.elpassion.android.commons.recycler.adapters.basicAdapterWithConstructors
 import com.elpassion.android.commons.recycler.basic.addAll
 import com.elpassion.android.commons.recycler.basic.asBasicListWithMutableSections
@@ -22,12 +23,12 @@ class BasicMutableRecyclerWithSectionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recycler_view_with_action)
-        recyclerView.layoutManager = android.support.v7.widget.LinearLayoutManager(this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
         val adapter = basicAdapterWithConstructors(users) { position ->
-            if (users[position].organization == "A")
-                R.layout.github_item to ::SimpleUserViewHolder
-            else
-                R.layout.other_github_item to ::OtherSimpleUserViewHolder
+            when (users[position].organization) {
+                "A" -> R.layout.github_item to ::SimpleUserViewHolder
+                else -> R.layout.other_github_item to ::OtherSimpleUserViewHolder
+            }
         }
         adapter.setHasStableIds(true)
         recyclerView.adapter = adapter
